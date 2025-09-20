d5ba434823d8b062306d7f5d9acbf43d93d84991
iPivovarov <pivovarov.vany@yandex.ru>
2025-09-20 21:31:45 +0300

    Добавил класс cat

---
+++ GitTestProject/Classes/Cat.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitTestProject.Classes
+{
+    internal class Cat
+    {
+        public string Name { get; set; } = string.Empty;
+    }
+}
